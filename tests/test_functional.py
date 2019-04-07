a0b43a2d4f761009a3881398ef76b21ce0ffbd8e
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-04-08 00:33:41 +0200

    adding basic functional testing

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -29,13 +29,31 @@ def test_sudoku_gen_hard():
     assert generated_game_hard == 0
 
 
-def test_sudoku_solve_txt():
+def test_sudoku_solve_txt_easy():
     """
-    lets solve a text file input sudoku game
+    lets solve an easy level text file input sudoku game
     """
-    target_file = os.path.join(os.getcwd(), 'tests', 'sudoku_test.txt')
-    solved_txt_game = solver(target_file, True)
-    assert solved_txt_game == 0
+    target_file = os.path.join(os.getcwd(), 'tests', 'files', 'sudoku_easy.txt')
+    solved_txt_game_easy = solver(target_file, True)
+    assert solved_txt_game_easy == 0
+
+
+def test_sudoku_solve_txt_medium():
+    """
+    lets solve a medium level text file input sudoku game
+    """
+    target_file = os.path.join(os.getcwd(), 'tests', 'files', 'sudoku_medium.txt')
+    solved_txt_game_medium = solver(target_file, True)
+    assert solved_txt_game_medium == 0
+
+
+def test_sudoku_solve_txt_hard():
+    """
+    lets solve a hard level text file input sudoku game
+    """
+    target_file = os.path.join(os.getcwd(), 'tests', 'files', 'sudoku_hard.txt')
+    solved_txt_game_hard = solver(target_file, True)
+    assert solved_txt_game_hard == 0
 
 
 #def test_sudoku_solve_ocr():
