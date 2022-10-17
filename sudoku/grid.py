406e37c873989eaa16e543c2bea59ac2092ab881
datahappy1 <p.prudky@seznam.cz>
2022-10-18 01:00:10 +0200

    optimalize square calculation logic

--- sudoku/grid.py
+++ sudoku/grid.py
@@ -6,6 +6,8 @@ from typing import List, Set, Tuple
 ALL_CANDIDATES_SET = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 # {row_index:(related_row_index1, related_row_index2),}
+# or
+# {col_index:(related_col_index1, related_col_index2),}
 GENERIC_GRID_MAP = {
     0: (1, 2),
     1: (2, 0),
@@ -29,11 +31,11 @@ def get_col_from_grid_rows(grid_rows: List[List[int]], col_index: int) -> Set[in
     return set(row[col_index] for row in grid_rows)
 
 
-def _get_related_rows_for_row_index(index: int) -> Tuple[int, int]:
+def _get_generic_grid_map_values_for_index(index: int) -> Tuple[int, int]:
     """
     grid offset mapping function
-    this function takes row index and returns
-    the related rows
+    this function takes row/col index and returns
+    the related rows/cols
     :param index:
     :return: related_row_index1, related_row_index2
     """
@@ -45,16 +47,16 @@ def get_square_from_position(
 ) -> Set[int]:
     """
     get square from position on grid except for the grid members from
-    current row provided by the row_index argument.
-    i.e when row_index == 5 and col_index == 3, function returns
-    a set containing the members marked as `x` :
+    current row and col provided by the row_index/col_index arguments.
+    for illustration, when provided row_index == 5 and col_index == 3 (marked below as `*`),
+    this function returns a set containing the square members (marked below as `x`):
 
     =========
     =========
     =========
-    ===xxx===
-    ===xxx===
-    ===X=====
+    ====xx===
+    ====xx===
+    ===*=====
     =========
     =========
     =========
@@ -66,30 +68,20 @@ def get_square_from_position(
     :return:
     """
     square = set()
-    square_column_boundaries = None, None
-
-    if col_index // 3 == 0:
-        square_column_boundaries = 0, 3
-    elif col_index // 3 == 1:
-        square_column_boundaries = 3, 6
-    elif col_index // 3 == 2:
-        square_column_boundaries = 6, 9
+    square_rows_indexes = _get_generic_grid_map_values_for_index(index=row_index)
+    square_column_indexes = _get_generic_grid_map_values_for_index(index=col_index)
 
     try:
-        for pos in grid_rows[_get_related_rows_for_row_index(row_index)[0]][
-            slice(square_column_boundaries[0], square_column_boundaries[1])
-        ]:
-            square.add(pos)
+        square.add(grid_rows[square_rows_indexes[0]][square_column_indexes[0]])
+        square.add(grid_rows[square_rows_indexes[0]][square_column_indexes[1]])
     except IndexError:
         # this is expected when generating grid, index error is raised for square
         # data lookups that are targeting rows/columns that haven't been yet populated
         pass
 
     try:
-        for pos in grid_rows[_get_related_rows_for_row_index(row_index)[1]][
-            slice(square_column_boundaries[0], square_column_boundaries[1])
-        ]:
-            square.add(pos)
+        square.add(grid_rows[square_rows_indexes[1]][square_column_indexes[0]])
+        square.add(grid_rows[square_rows_indexes[1]][square_column_indexes[1]])
     except IndexError:
         # this is expected when generating grid, index error is raised for square
         # data lookups that are targeting rows/columns that haven't been yet populated
